c33907fec1b67957f93dd95e8d9eafe7d5b1c4b4
ibutsky <iryna.butsky@gmail.com>
2011-06-27 14:05:08 -0700

    Saving Example01 and Assignment01

---
+++ Iryna/Assignment01Iryna/libAssignment01Iryna.hpp
@@ -0,0 +1,11 @@
+/**
+ * Takes three arguments. Adds the third argument to the product of the first
+ * two.
+ *
+ * @param a  the first argument
+ * @param b  the second argument
+ * @param c  the third argument
+ * @return   a*b + c
+ */
+
+double MultiplyAdd(const double a, const double b, const double c); 

d3e360dd66e2f92b7bd5f07e1ade098dec497b35
Curran D. Muhlberger <cdmuhlb@gmail.com>
2011-06-28 16:14:00 -0400

    Added Curran's personal directory.
    Initial commit includes Example01 and Assignment01.

---
+++ Curran/Assignment01Curran/MultiplyAddCurran.hpp
@@ -0,0 +1,10 @@
+/**
+ * Performs a multiply-add operation (A*B + C) on three operands.
+ *
+ * @param a     the first factor in the multiplication
+ * @param b     the second factor in the multiplication
+ * @param c     the second term in the sum
+ * @return      the quantity a*b+c
+ */
+double MultiplyAdd(double a, double b, double c);
+

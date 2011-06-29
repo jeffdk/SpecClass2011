1dee0e713d6102178245f4107d678767c64f8a75
Brett Deaton <brett.deaton@gmail.com>
2011-06-28 21:37:13 -0700

    I created Brett's SpEC class 2011 project folder. I added two libraries: Example01Brett, and Assignment01Brett.

---
+++ BrettDeaton/Example01Brett/Executables/MyExecWithInput.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "Round.hpp"
+#include "StringConversion.hpp"
+#include "OptionParser.hpp"
+#include "ReadFileIntoString.hpp"
+#include "MyFirstLibBrett.hpp"
+
+int main(int /*argc*/, char** /*argv*/) {
+    std::cout << "Hello world!" << std::endl;
+
+    std::string opts = ReadFileIntoString("ImposeCap.input");
+    OptionParser parser(opts);
+    double x = parser.Get<double>("Value");
+    double cap = parser.Get<double>("Cap");
+    double x_capped = ImposeCap(x, cap);
+    std::cout << "x is " << x << ", cap is " << cap << std::endl;
+    std::cout << "The capped value of x is " << x_capped << std::endl;
+
+    return EXIT_SUCCESS;
+}
+

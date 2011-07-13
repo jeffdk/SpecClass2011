e4ac0b8b215d7a3c8cfc89eeca241266fcb1e8bc
Curran D. Muhlberger <cdmuhlb@gmail.com>
2011-07-13 17:02:42 -0400

    Finished required portion of homework 3.

--- Curran/Assignment03Curran/Executables/HomeworkTensor.cpp
+++ Curran/Assignment03Curran/Executables/HomeworkTensor.cpp
@@ -1,17 +1,39 @@
 #include <cstdlib>
+#include <cmath>
 #include <iostream>
 
+#include "MatrixUtils3x3Curran.hpp"
 #include "DataMesh.hpp"
 #include "Tensor.hpp"
-#include "MatrixUtils3x3Curran.hpp"
+
+bool AreEqualAbsRel(const double a, const double b,
+                    const double atol, const double rtol) {
+    return fabs(a - b) < atol + 0.5*(fabs(a) + fabs(b))*rtol;
+}
+
+bool AreEqualAbsRel3x3Sym(const Tensor<DataMesh>& a, const Tensor<DataMesh>& b,
+                          const double atol, const double rtol) {
+    const int dim = 3;
+    for (int i = 0; i < dim; ++i) {
+        for (int j = i; j < dim; ++j) {
+            for (int idx = 0; idx < a(i,j).Size(); ++idx) {
+                if (!AreEqualAbsRel(a(i,j)[idx], b(i,j)[idx], atol, rtol)) {
+                    return false;
+                 }
+            }
+        }
+    }
+    return true;
+}
 
 int main(int /*argc*/, char** /*argv*/) {
     using namespace Assignment03Curran;
 
-    { // First block
+    const int dim = 3;
+
+    { // Single matrix with trivial inverse
     const IPoint extents(MV::fill, 1, 1, 1);
     const Mesh mesh(extents);
-    const int dim = 3;
     Tensor<DataMesh> mat(dim, "aa", mesh);
     mat(0,0) = 2.0;
     mat(0,1) = 0.0;
@@ -24,13 +46,18 @@ int main(int /*argc*/, char** /*argv*/) {
     Tensor<DataMesh> orig2 = Inverse3x3Sym(inv);
     Tensor<DataMesh> prod = Multiply3x3Sym(mat, inv);
 
+    // Print out matrices for inspection by eye
     std::cout << mat << std::endl;
     std::cout << orig2 << std::endl;
     std::cout << inv << std::endl;
     std::cout << prod << std::endl;
-    } // First block
 
-    { // Second block
+    // Compare mat with inv(inv(mat))
+    const bool success = AreEqualAbsRel3x3Sym(mat, orig2, 1.0e-12, 1.0e-12);
+    std::cout << (success ? "Success" : "Failure") << std::endl;
+    }
+
+    { // Random tensor on 10x10 mesh
     const IPoint extents(MV::fill, 10, 10);
     const Mesh mesh(extents);
     const int dim = 3;
@@ -38,16 +65,14 @@ int main(int /*argc*/, char** /*argv*/) {
     Random3x3Sym(12345, mat);
 
     Tensor<DataMesh> inv = Inverse3x3Sym(mat);
-    Tensor<DataMesh> orig2 = Inverse3x3Sym(inv);
-    Tensor<DataMesh> prod = Multiply3x3Sym(mat, inv);
+    Tensor<DataMesh> invinv = Inverse3x3Sym(inv);
 
-    std::cout << mat << std::endl;
-    std::cout << orig2 << std::endl;
-    std::cout << inv << std::endl;
-    std::cout << prod << std::endl;
-    } // Second block
+    // Compare mat with inv(inv(mat))
+    const bool success = AreEqualAbsRel3x3Sym(mat, invinv, 1.0e-12, 1.0e-12);
+    std::cout << (success ? "Success" : "Failure") << std::endl;
+    }
 
-    { // Third block
+    { // Random tensor on 3x3x3 mesh
     const IPoint extents(MV::fill, 3, 3, 3);
     const Mesh mesh(extents);
     const int dim = 3;
@@ -55,14 +80,12 @@ int main(int /*argc*/, char** /*argv*/) {
     Random3x3Sym(67890, mat);
 
     Tensor<DataMesh> inv = Inverse3x3Sym(mat);
-    Tensor<DataMesh> orig2 = Inverse3x3Sym(inv);
-    Tensor<DataMesh> prod = Multiply3x3Sym(mat, inv);
+    Tensor<DataMesh> invinv = Inverse3x3Sym(inv);
 
-    std::cout << mat << std::endl;
-    std::cout << orig2 << std::endl;
-    std::cout << inv << std::endl;
-    std::cout << prod << std::endl;
-    } // Third block
+    // Compare mat with inv(inv(mat))
+    const bool success = AreEqualAbsRel3x3Sym(mat, invinv, 1.0e-12, 1.0e-12);
+    std::cout << (success ? "Success" : "Failure") << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }

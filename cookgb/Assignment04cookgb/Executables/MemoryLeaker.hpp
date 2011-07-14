c7fbdfd04f872d94e1f6a29962ab78ae11b6789e
Greg Cook <cookgb@wfu.edu>
2011-07-14 11:09:01 -0400

    Done with Problem 3 of Assignement 4

---
+++ cookgb/Assignment04cookgb/Executables/MemoryLeaker.hpp
@@ -0,0 +1,13 @@
+class MemoryLeaker {
+public:
+  MemoryLeaker(const int n) 
+    : pLeaky(new double[n]) {}
+  ~MemoryLeaker() {delete[] pLeaky;}
+  void Reset(const int n) {
+    delete[] pLeaky;
+    pLeaky = new double[n];
+  }
+private:
+  double * pLeaky;  
+};
+

ae73bad7e080f23ddc5795ee5de9e1c6ffc68589
Curran D. Muhlberger <cdmuhlb@gmail.com>
2011-07-19 23:21:07 -0400

    Started assignment 5.

---
+++ Curran/Assignment05Curran/Executables/Problem2Exec.cpp
@@ -0,0 +1,46 @@
+#include <cstdlib>
+#include <cmath>
+#include <string>
+
+#include "MyVector.hpp"
+#include "ReadFileIntoString.hpp"
+#include "OptionParser.hpp"
+#include "BasicMpi.hpp"
+#include "OneDimDataWriter.hpp"
+
+void ComputeProfileAndOutput(const double t, 
+                             const OneDimDataWriter* writer) {
+    const int N = 1024;
+    MyVector<double> x(MV::Size(2*N+1)), y(MV::Size(2*N+1));
+    for(int i = 0; i <= 2*N; ++i) {
+        x[i] = 0.01*(i-N);
+        y[i] = exp(-x[i]*x[i]/(t*t)); // heat-diffusion
+    }
+    writer->AppendToFile(t, x, y);
+}
+
+int main(int argc, char** argv) {
+    MpiInit(&argc, &argv);
+
+    const std::string helptxt =
+"OneDFormat.input:                                                       \n"
+"Example input file for Lecture 5                                        \n"
+"Format   = string;               # Which 1-D output format              \n"
+"Filename = string;               # Filename                             \n"
+"Times    = double, double, ...;  # Which times to output                \n";
+ 
+    const std::string options = ReadFileIntoString("OneDFormat.input");
+    OptionParser parser(options, helptxt);
+    const std::string format   = parser.Get<std::string>("Format");
+    const std::string filename = parser.Get<std::string>("Filename");
+    MyVector<double> times     = parser.Get<MyVector<double> >("Times");
+ 
+    OneDimDataWriter* pWriter = OneDimDataWriter::Create(format, filename);
+    for(int i = 0; i < times.Size(); ++i) {
+        ComputeProfileAndOutput(times[i], pWriter);
+    }
+    delete pWriter;
+
+    return EXIT_SUCCESS;
+}
+

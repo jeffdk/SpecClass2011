df4ac3246afdbe0a41792042f239ef0462115996
Hemberger <daniel.hemberger@gmail.com>
2011-07-26 19:01:44 -0400

    Adding assignments.

---
+++ danh/Assignment04danh/TotalVolumeOfSpheresFromRadii_danh.cpp
@@ -0,0 +1,11 @@
+#include "TotalVolumeOfSpheresFromRadii_danh.hpp"
+#include "MyVector.hpp"
+
+double TotalVolumeOfSpheresFromRadii(const MyVector<double>& radii) {
+  const double Pi = 3.1415926535897932;
+  double TotalVolume = 0.0;
+  for(int i=0; i<radii.Size(); ++i) {
+     TotalVolume += (4./3.)*Pi*radii[i]*radii[i]*radii[i];
+  }
+  return TotalVolume;
+}

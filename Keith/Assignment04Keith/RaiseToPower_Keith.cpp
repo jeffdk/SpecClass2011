18a6ef5e3eb0aa5bcdb3407fd05f60536fd8f9f1
Keith <keith@alice.caltech.edu>
2011-07-26 22:51:29 -0700

    Adding RaiseToPower_Keith.?pp along with associated executable source Buggy.cpp.  Modified Makefiles.  Adding Problem2Errors.txt.

---
+++ Keith/Assignment04Keith/RaiseToPower_Keith.cpp
@@ -0,0 +1,8 @@
+#include "RaiseToPower_Keith.hpp"
+// Compute a^n
+unsigned int RaiseToPower(const unsigned int a,  const unsigned int n) {
+  if(n==0) return 1;
+  if(n>1) return a*RaiseToPower(a,n-1);
+  return a;
+}
+

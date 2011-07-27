43e994d5fadc1b1cea62ea01d308783a227425e3
Keith <keith@alice.caltech.edu>
2011-07-26 21:30:06 -0700

    Added to Problem1Errors.txt.  Rewrote Volume_Keith.?pp to fix those errors. Adding .gitignore to Assignment04Keith directory.

--- Keith/Assignment04Keith/Volume_Keith.hpp
+++ Keith/Assignment04Keith/Volume_Keith.hpp
@@ -1,2 +1,12 @@
-#include "MyVector.hpp" 
+/// \file
+/// Defines function TotalVolumeOfSpheresFromRadii.
+#ifndef Volume_Keith_hpp
+#define Volume_Keith_hpp
+
+template <typename> class MyVector;
+
+/// Computes the sum of volumes of spheres whose radii are the 
+/// components the MyVector argument.
 double TotalVolumeOfSpheresFromRadii(const MyVector<double>& radii);
+
+#endif

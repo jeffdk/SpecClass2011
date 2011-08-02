804e26e5863477d55e443606f0488e383c6f40f1
Ilana MacDonald <macdonald@astro.utoronto.ca>
2011-08-02 18:57:00 -0400

    fixed errors in assn 4

---
+++ Ilana/Assignment05Ilana/OutputReducedDatIlana.cpp
@@ -0,0 +1,12 @@
+#include "OutputReducedDatIlana.hpp"
+#include "Utils/MyContainers/MyVectore.hpp"
+#include "OneDimDataWriter.hpp"
+#include "Utils/StringParsing/OptionParser.hpp"
+#include "NdData.hpp"
+#include "Utils/StringParsing/StringUtils.hpp"
+#include "Sex
+
+#include <cmath>
+#include <fstream>
+
+namespace OneDimDataWriters

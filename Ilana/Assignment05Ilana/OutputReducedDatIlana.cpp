f22009766acef12a736f328af9a6315cb3c8094d
Ilana MacDonald <macdonald@astro.utoronto.ca>
2011-08-02 23:54:18 -0400

    small changes to assn 4

--- Ilana/Assignment05Ilana/OutputReducedDatIlana.cpp
+++ Ilana/Assignment05Ilana/OutputReducedDatIlana.cpp
@@ -1,5 +1,5 @@
 #include "OutputReducedDatIlana.hpp"
-#include "Utils/MyContainers/MyVectore.hpp"
+#include "Utils/MyContainers/MyVector.hpp"
 #include "OneDimDataWriter.hpp"
 #include "Utils/StringParsing/OptionParser.hpp"
 #include "NdData.hpp"

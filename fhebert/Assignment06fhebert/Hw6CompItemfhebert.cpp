99f3a1a1e0c5c53faeb40c55b2862debc204dd55
Francois Hebert <fmh33@cornell.edu>
2011-08-03 02:51:45 -0400

    fix to assignment 6

--- fhebert/Assignment06fhebert/Hw6CompItemfhebert.cpp
+++ fhebert/Assignment06fhebert/Hw6CompItemfhebert.cpp
@@ -20,20 +20,19 @@ namespace ComputeItems {
     const MyVector<DataMesh>& Coords =
         box.Get<MyVector<DataMesh> >("GlobalCoords");
 
-    //DataMesh RSqrd(Coords[0], 0.0);
-
-    // add squares of the DMs
-    //for (int dim=0; dim<Coords.Size(); ++dim)
-    //{
-    //  RSqrd += Coords[dim]*Coords[dim];
-    //}
+    // convert from x,y,z to r
+    DataMesh RSqrd(Coords[0], 0.0);
+    for (int dim=0; dim<Coords.Size(); ++dim)
+    {
+      RSqrd += Coords[dim]*Coords[dim];
+    }
 
     // give output on the same Mesh as the GlobalCoords:
     mResult.assign(Coords[0].Dim(), "a", Coords[0]);
 
     for (int dim=0; dim<Coords.Size(); ++dim)
     {
-      mResult(dim) = Coords[dim] / pow(Coords[dim], 3);
+      mResult(dim) = Coords[dim] / pow(RSqrd[dim], 3./2.);
     }
     
   }

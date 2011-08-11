bfa0c5ada5f4d204803054c95d1c77576098168a
Greg Cook <cookgb@wfu.edu>
2011-08-11 11:55:09 -0400

    Homework set 7

---
+++ cookgb/Assignment07cookgb/ObservePointwiseAvg.cpp
@@ -0,0 +1,55 @@
+#include "ObservePointwiseAvg.hpp"
+#include "Utils/DataBox/DataBox.hpp"
+#include "Utils/DataBox/DataBoxAccess.hpp"
+#include "Utils/Tensor/Tensor.hpp"
+#include "Utils/DataMesh/DataMesh.hpp"
+#include "Utils/DataMesh/DataMeshNorms.hpp"
+#include "Utils/IO/PrependDirectoryName.hpp"
+#include "Utils/StringParsing/OptionParser.hpp"
+#include "Utils/MiscUtils/MpiUtils.hpp"
+#include "Utils/MpiWrappers/MpiCommunicator.hpp"
+#include "Dust/Domain/Domain.hpp"
+#include <fstream>
+#include <string>
+ 
+namespace Observers {
+ 
+  PointwiseAvg::PointwiseAvg(const std::string &Opts,
+			     const DataBox&,const std::string& Dir) {
+    OptionParser p(Opts,Help());
+    mFileName = PrependDirectory(p.Get<std::string>("Filename"),Dir);
+    mInput    = p.Get<std::string>("Input");
+  }
+ 
+  void PointwiseAvg::Observe(const DataBox& box,
+		    const double /* TimeForDumpFiles */,
+		    const std::string& FirstColumnInDatFiles) const
+  {
+    // Compute norm
+    DataBoxAccess boxA(box,"Observers::PointwiseAvg::Observe");
+    const Domain& D = boxA.Get<Domain>("Domain");
+ 
+    double sum = 0.0;
+    int  count = 0;
+    for(int sd=0;sd<D.Size();++sd) {
+      const Tensor<DataMesh>& Input = boxA[sd].Get<Tensor<DataMesh> >(mInput);
+      for(Tensor<DataMesh>::const_iterator i=Input.begin();i!=Input.end();++i) {
+	const int component_size = i->Size();
+	const double * pdata = i->Data();
+	for(int j=0; j<component_size; ++j) sum += *pdata++;
+	count += component_size;
+      }
+    }
+    sum = MPreduceAdd(sum,D.Communicator());
+    count = MPreduceAdd(count,D.Communicator());
+    const double average = sum/count;
+ 
+    // Output the norm
+    if(D.Communicator().Rank()==0) {
+      std::ofstream out(mFileName.c_str(),std::ios::app);
+      out << FirstColumnInDatFiles << " " << average << std::endl;
+    }
+  }
+}
+ 
+REGISTER_OBSERVER(PointwiseAvg)

4e5724182edbd4567f3ad5e562af24a50b0ec09e
ibutsky <iryna.butsky@gmail.com>
2011-08-31 12:46:15 -0700

    fixing Assignment 05

--- Iryna/Assignment05Iryna/OutputReducedDatIryna.cpp
+++ Iryna/Assignment05Iryna/OutputReducedDatIryna.cpp
@@ -1,9 +1,8 @@
 #include "OutputReducedDatIryna.hpp"
 #include "Utils/StringParsing/OptionParser.hpp"
-//#include "NdData.hpp"
-//#include "StringUtils.hpp"
 #include "Utils/LowLevelUtils/ConvertNumberToString.hpp"
 #include "Utils/ErrorHandling/Require.hpp"
+#include "Utils/IO/CachedOfStream.hpp"
 
 #include <cstdlib>
 #include <fstream>
@@ -11,49 +10,63 @@
 
 namespace OneDimDataWriters {
 
- //namespace {
-     void OutputReducedDatIryna::
-     AppendToFileImpl(const double time, 
-       	              const MyVector<double>& x,
-		      const MyVector<double>& y) const {
+  OutputReducedDatIryna::OutputReducedDatIryna(const std::string& opts,
+                                               const std::string& BaseName):
+     mBaseName(BaseName){
+     OptionParser parser(opts, Help());
+     mMinAngle = parser.Get<double>("MinAngle");
+  }
+
+  void OutputReducedDatIryna::TruncateFile() const {
+  }
+
+  void OutputReducedDatIryna::
+         AppendToFileImpl(const double time, 
+         const MyVector<double>& x,
+         const MyVector<double>& y) const {
         
-	std::ostringstream outputFile;
-    	outputFile << mBaseName;
-    	//std::ofstream out(outputFile.str().c_str());
+     std::ostringstream outputFile;
+     outputFile << mBaseName << ".dat";
 
-        const int size_x = x.Size();
-	const int size_y = y.Size();
+     const int size_x = x.Size();
+     const int size_y = y.Size();
 	
-	REQUIRE(size_x == size_y, "Error: Data of x and y must have equal \
-					length");
-	// write first data point
-	outputFile << DoubleToString(x[0], 16) << ""
-	    << DoubleToString(y[0], 16) << "\n"; 
+     REQUIRE(size_x == size_y, "Error: Data of x and y must have equal \
+				length");
+     CachedOfStream outfile(outputFile.str());
 
-	if(size_x > 2){
-	   for(int i = 1; i < size_x -1; ++i){
+     if(size_x == 0){}
 
-	      double old_dx = x[i] - x[i-1];
-	      double old_dy = y[i] - y[i-1];
-	      double old_angle = atan2(old_dy, old_dx); 
+     if(size_x > 0){	
+        // write first data point
+        outfile << DoubleToString(x[0], 16) << ""
+                << DoubleToString(y[0], 16) << "\n"; 
 
-	      double dx = x[i+1] - x[i];
-	      double dy = y[i+1] - y[i];
-	      double angle = atan2(dy, dx);
+        if(size_x > 2){
+           for(int i = 1; i < size_x -1; ++i){
 
-	      if(angle - old_angle > mMinAngle || old_angle - angle > mMinAngle){
-  	         outputFile << DoubleToString(x[i], 16) << ""
-	             << DoubleToString(y[i], 16) << "\n"; 
-	      }
+              double old_dx = x[i] - x[i-1];
+              double old_dy = y[i] - y[i-1];
+              double old_angle = atan2(old_dy, old_dx); 
+
+              double dx = x[i+1] - x[i];
+              double dy = y[i+1] - y[i];
+              double angle = atan2(dy, dx);
+
+              if(angle - old_angle > mMinAngle || old_angle - angle > 
+                                                     mMinAngle){
+                 outfile << DoubleToString(x[i], 16) << ""
+	                << DoubleToString(y[i], 16) << "\n"; 
+              }
 	 
            }
 	}
 
-	// plot last point
-	outputFile << DoubleToString(x[size_x -1], 16) << ""
-	    << DoubleToString(y[size_y -1], 16) << "\n"; 
+        // plot last point
+        outfile << DoubleToString(x[size_x -1], 16) << ""
+                 << DoubleToString(y[size_y -1], 16) << "\n"; 
+     }	
+	
+  }   
+}  
 
-
-     }
-  }  
-//}
--- Iryna/Assignment05Iryna/OutputReducedDatIryna.hpp
+++ Iryna/Assignment05Iryna/OutputReducedDatIryna.hpp
@@ -1,13 +1,11 @@
-#include "Utils/StringParsing/OptionParser.hpp"
+#ifndef ASSIGNMENT05IRYNA_OUTPUTREDUCEDDATIRYNA_HPP_
+#define ASSIGNMENT05IRYNA_OUTPUTREDUCEDDATIRYNA_HPP_
+
 #include "Utils/IO/OneDimDataWriter.hpp"
 #include "Utils/MyContainers/MyVector.hpp"
-#include "Utils/IO/CachedOfStream.hpp"
 
 #include <string>
-#include <cstdlib>
-#include <fstream>
-#include <cmath>
-#include <iostream>
+
 
 namespace OneDimDataWriters {
 
@@ -15,24 +13,26 @@ namespace OneDimDataWriters {
      Factory::Register<OutputReducedDatIryna>
   {
      public: 
-	static std::string ClassID() {return "rdatIryna";}
-	static std::string Help() {
+        static std::string ClassID() {return "rdatIryna";}
+        static std::string Help() {
            return ClassID() + "\n"
               "Output: Two columns containing x_i and y_i with each i-th    \n"
-	      "data point omitted.                                          \n"
-	      "OPTIONS: MinAngle = double;  if abs(180 - angle) < MinAngle, \n"
-	      "discard the i-th data point. \n" ;
+              "data point omitted.                                          \n"
+              "OPTIONS: MinAngle = double;  if abs(180 - angle) < MinAngle, \n"
+              "discard the i-th data point. \n" ;
         }
         OutputReducedDatIryna(const std::string& opts, const std::string& BaseName);
     
-	void TruncateFile() const; 
+        void TruncateFile() const; 
 
      private:
         const std::string mBaseName;
         double mMinAngle;
 
-	void AppendToFileImpl(const double time, 
-			      const MyVector<double>& x,
-			      const MyVector<double>& y) const;
+        void AppendToFileImpl(const double time, 
+                              const MyVector<double>& x,
+                              const MyVector<double>& y) const;
      };
 };
+
+#endif // ASSIGNMENT05IRYNA_OUTPUTREDUCEDDATIRYNA_HPP_
